2bad6abb1e68f93bdd3b5accca606451299fdd99
brwitten <bethrwitten@gmail.com>
2017-10-12 12:12:38 -0700

    added in filters for route

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,6 +2,11 @@ console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
 
-// your code
-
+  $.ajax({
+    method: "GET",
+    url: "http://localhost:3000/api/routes",
+    success: function(json){
+      console.log(json)
+    }
+  })
 });
